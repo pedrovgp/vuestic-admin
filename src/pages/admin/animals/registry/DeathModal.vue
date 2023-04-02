8169125b6c20882b96ba1fe375ef826196323c6c
pv <pedrovgp@gmail.com>
2023-04-02 12:47:19 -0300

    ADD death modal form

--- src/pages/admin/animals/registry/DeathModal.vue
+++ src/pages/admin/animals/registry/DeathModal.vue
@@ -7,21 +7,20 @@ If it does not, it pre fills some fields (like animalId, date with todays date) 
   <va-button color="danger" icon="Skull" @click="showContent = !showContent">
     {{ props.buttonText || 'Morte' }}
   </va-button>
-  <Teleport to="body">
-    <va-modal v-model="showContent" :title="'Morte'">
-      <template #content="{ ok }">
-        <va-form v-model="formValid" @submit="submitForm">
-          <va-card-content>
-            <va-input v-model="animalId" :label="'animal'" :readonly="true" />
-            <va-date-picker v-model="date" :label="$t('date')" :format="formatFn" />
-            <va-input v-model="cause" :label="'cause'" />
-            <va-input v-model="obs" :label="'obs'" />
-            <va-button type="submit" color="danger" :disabled="!formValid" @click="ok"> save </va-button>
-          </va-card-content>
-        </va-form>
-      </template>
-    </va-modal>
-  </Teleport>
+  <va-modal v-model="showContent">
+    <template #content="{ ok }">
+      <va-card-title> Registrando a morte do animal </va-card-title>
+      <va-form v-model="formValid" @submit="submitForm">
+        <va-card-content>
+          <va-input v-model="animalId" :label="'animal'" :readonly="true" />
+          <va-select v-model="cause" :options="causeOptions" label="Causa da morte" />
+          <va-date-input v-model="date" label="Data da morte (ANO-MÊS-DIA)" :format="formatFn" />
+          <va-input v-model="obs" label="Observação (opcional)" />
+          <va-button type="submit" color="danger" :disabled="!formValid" @click="ok"> Salvar </va-button>
+        </va-card-content>
+      </va-form>
+    </template>
+  </va-modal>
 </template>
 
 <script setup lang="ts">
@@ -35,11 +34,18 @@ If it does not, it pre fills some fields (like animalId, date with todays date) 
 
   const animalId = ref(props.animalId)
   // Use todays date as default, converted to ISO string date format
-  const date = ref(new Date().toISOString().split('T')[0])
-  const cause = ref('')
+  const date = ref(new Date())
+  const cause = ref('DOENCA')
   const obs = ref('')
   const formValid = ref(false)
   const showContent = ref(false)
+
+  // Options for cause of death: NATURAL, DOENCA ou ACIDENTE
+  const causeOptions = ['DOENCA', 'ACIDENTE', 'NATURAL']
+  //   { value: 'NATURAL', text: 'Natural' },
+  //   { value: 'DOENCA', text: 'Doença' },
+  //   { value: 'ACIDENTE', text: 'Acidente' },
+  // ]
 
   function formatFn(date: any) {
     return date.toISOString().split('T')[0]
